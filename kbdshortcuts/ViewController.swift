dae7908ed28fd225a5ba3a2ec89057b49c1982b7
Zero King <l2dy@icloud.com>
2024-03-17 20:21:29 +0800

    Initial Commit

---
+++ kbdshortcuts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kbdshortcuts
+//
+//  Created by l2dy on 3/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
